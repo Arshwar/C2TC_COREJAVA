47be2ed35e9490b06cefe3e74d774cc05707e91a
Arshad <Arshad@DESKTOP-T711D0V>
2024-10-25 15:06:38 +0530

    demo

---
+++ C2TC_COREJAVA/src/com/ifet/dayone/IdentifersDemo.java
@@ -0,0 +1,14 @@
+package com.ifet.dayone;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int num= 57;
+			System.out.println("integer value:"+num);
+			String name="Arshad";
+			System.out.println("String Name:"+name);
+			
+	}
+
+}
