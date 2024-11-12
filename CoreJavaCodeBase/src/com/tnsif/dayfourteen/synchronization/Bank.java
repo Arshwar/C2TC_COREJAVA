397b78093c0403fefa8a46dca2989c025544f4bf
Arshad <143896297+Arshwar@users.noreply.github.com>
2024-11-12 20:27:25 +0530

    Delete CoreJavaCodeBase directory

--- CoreJavaCodeBase/src/com/tnsif/dayfourteen/synchronization/Bank.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.dayfourteen.synchronization;
-
-public interface Bank {
-	int MINBAL=5000; 
-	static final int DAILY_LIMIT=25000;
-	public abstract void deposit(int amt) throws DepositLimitExceedsException;
-	void withdraw(int amt)throws InsufficientBalanceException; //public and abstract
-}
